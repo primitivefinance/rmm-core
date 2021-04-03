a1a2021aa873c4dc6b94dc9ceedeb65e7ff2c59e
Alexangelj <alexangel.j@gmail.com>
2021-04-03 11:29:39 -0700

    feat(tiered): Adds the tiered engine functions for math invariants.

--- contracts/engines/Tier1Engine.sol
+++ contracts/engines/Tier1Engine.sol
@@ -10,9 +10,25 @@ pragma solidity 0.8.0;
 import "../libraries/LogitMath.sol";
 
 abstract contract Tier1Engine {
-    function calculateInvariant() internal pure returns (int128) {}
+    function calculateInvariant(uint p, int slope, int translation) internal pure returns (int128 invariant) {
+        invariant = LogitMath.calcInvariant(p, slope, translation);
+    }
 
-    function calculateInput() internal pure returns (int128) {}
+    /**
+     * @notice  Swap Y -> X. Calculates the amount of Y that must enter the pool to preserve the invariant.
+     * @dev     X leaves the pool.
+     */
+    function calculateInput(uint deltaX, uint RX1, uint RY2, uint liquidity, LogitMath.Params memory params) internal pure returns (int128 deltaY) {
+        RX1 = RX1 - deltaX;
+        deltaY = LogitMath.getTradingFunction(RX1, RY2, liquidity, params);
+    }
 
-    function calculateOutput() internal pure returns (int128) {}
+    /**
+     * @notice  Swap X -> Y. Calculates the amount of Y that must leave the pool to preserve the invariant.
+     * @dev     X enters the pool.
+     */
+    function calculateOutput(uint deltaX, uint RX1, uint RY2, uint liquidity, LogitMath.Params memory params) internal pure returns (int128 deltaY) {
+        RX1 = RX1 + deltaX;
+        deltaY = LogitMath.getTradingFunction(RX1, RY2, liquidity, params);
+    }
 }
