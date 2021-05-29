6353d9c911cdebbe158400920a8970f074dd8485
Alexangelj <alexangel.j@gmail.com>
2021-05-28 22:17:33 -0700

    Adds natspec to Transfers lib.

--- contracts/libraries/Transfers.sol
+++ contracts/libraries/Transfers.sol
@@ -5,10 +5,15 @@ import "../interfaces/IERC20.sol";
 
 library Transfers {
     /// @notice Performs an ERC20 `transfer` call and checks return data
+    /// @param  token   The ERC20 token to transfer
+    /// @param  to      The recipient of the ERC20 token
+    /// @param  value   The amount of ERC20 to transfer
+    /// @return Whether or not the call was successful
     function safeTransfer(IERC20 token, address to, uint value) internal returns (bool) {
         (bool success, bytes memory returnData) = address(token).call(
             abi.encodeWithSelector(token.transfer.selector, to, value)
         );
-        require(success && (returnData.length == 0 || abi.decode(returnData, (bool))), "Transfer fail");  
+        require(success && (returnData.length == 0 || abi.decode(returnData, (bool))), "Transfer fail");
+        return success;
     }
 }
