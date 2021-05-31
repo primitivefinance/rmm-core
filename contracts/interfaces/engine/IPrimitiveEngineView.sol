fe400188d9b73ba87e15487135a82cf86a879cf7
Kinrezc <matt.czernik@gmail.com>
2021-05-31 15:16:15 -0400

    Fix natspec compiler errors

--- contracts/interfaces/engine/IPrimitiveEngineView.sol
+++ contracts/interfaces/engine/IPrimitiveEngineView.sol
@@ -15,7 +15,7 @@ interface IPrimitiveEngineView {
     function compute(bytes32 pid, address token, uint reserve) external view returns (int128 reserveOfToken);
     
     /// @notice Uses the trading function to calculate an invariant using risky and stable token reserve values
-    /// @param  pid
+    /// @param  pid The hashed pool Id
     /// @param  postR1 The amount of risky tokens in the pool's reserves
     /// @param  postR2 The amount of stable tokens in the pool's reserves
     /// @param  postLiquidity The total supply of liquidity shares for the pool
@@ -43,7 +43,15 @@ interface IPrimitiveEngineView {
     // ===== Pool States =====
     /// @notice Fetches the global reserve state for a pool with `pid`
     /// @param  pid The pool id hash
-    /// @return TODO: Finish this
+    /// @return RX1 risky balance
+    /// @return RY2 risk free balance
+    /// @return liquidity total liquidity shares
+    /// @return float liquidity shares available to be borrowed 
+    /// @return debt total borrow liquidity shares
+    /// @return cumulativeRisky tracks cumulative risky reserves overtime
+    /// @return cumulativeStable tracks cumulative stable reserves overtime
+    /// @return cumulativeLiquidity tracks cumulative liquidity factor overtime
+    /// @return blockTimestamp unix timestamp when the cumulative reserve values were last updated
     function reserves(bytes32 pid) external view returns (
         uint RX1, uint RY2, uint liquidity, uint float, uint debt, uint cumulativeRisky,
         uint cumulativeStable,
@@ -60,7 +68,13 @@ interface IPrimitiveEngineView {
 
     /// @notice Fetches The position data struct using a position id
     /// @param  posId   The position id
-    /// @return TODO: Finish this
+    /// @return owner   the address that owns the position
+    /// @return pid     The pool id hash
+    /// @return balanceX The risky balance of the position debt
+    /// @return balanceY The stable balance of the position debt
+    /// @return liquidity The liquidity shares in the position
+    /// @return float   The liquidity shares that are marked for loans
+    /// @return debt    The position debt denominated in liquidity shares
     function positions(bytes32 posId) external view returns (
         address owner, bytes32 pid, uint balanceX, uint balanceY, uint liquidity, uint float, uint debt
     );
@@ -69,7 +83,7 @@ interface IPrimitiveEngineView {
     /// @param  owner   The margin account's owner
     /// @return BX1     The balance of the risky token
     /// @return BY2     The balance of the stable token
-    /// @return unlocked If the margin position is unlocked, only during execution is it locked. TODO: Fix 
+    /// @return unlocked If the margin position is unlocked, only during execution is it locked.
     function margins(address owner) external view returns (uint BX1, uint BY2, bool unlocked);
 
     /// @param  strike  The strike price of the pool
@@ -78,6 +92,6 @@ interface IPrimitiveEngineView {
     /// @return The keccak256 hash of the `calibration` parameters
     function getPoolId(uint strike, uint sigma, uint time) external view returns(bytes32);
 
-    /// @return The length of the all pool ids array
+    /// @return len The length of the all pool ids array
     function getAllPoolsLength() external view returns (uint len);
 }
