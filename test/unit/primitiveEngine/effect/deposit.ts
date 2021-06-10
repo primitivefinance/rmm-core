7a9c819db7bd67fef965b362986a6359f0142a9a
clemlak <clementlakhal@gmail.com>
2021-06-10 17:58:25 +0200

    feat: Update tests

--- test/unit/primitiveEngine/effect/deposit.ts
+++ test/unit/primitiveEngine/effect/deposit.ts
@@ -1,6 +1,8 @@
 import { waffle } from 'hardhat'
 import { expect } from 'chai'
-import { constants, utils, } from 'ethers'
+import { constants } from 'ethers'
+
+import { parseWei } from '../../../shared/Units'
 
 import {
   depositFragment,
@@ -9,8 +11,6 @@ import {
 import { loadContext } from '../../context'
 
 describe('deposit', function () {
-  let engineAddress: string;
-
   before(async function () {
     loadContext(
       waffle.provider,
@@ -19,50 +19,43 @@ describe('deposit', function () {
     );
   });
 
-  beforeEach(async function () {
-    engineAddress = await this.contracts.factory.getEngine(
-      this.contracts.risky.address,
-      this.contracts.stable.address
-    )
-  })
-
   describe('when the parameters are valid', function () {
     it('adds to the user margin account', async function () {
       await this.contracts.engineDeposit.deposit(
-        engineAddress,
+        this.contracts.engine.address,
         this.contracts.risky.address,
         this.contracts.stable.address,
         this.signers[0].address,
-        utils.parseEther('1000'),
-        utils.parseEther('1000'),
+        parseWei('1000').raw,
+        parseWei('1000').raw,
       );
 
       expect(await this.contracts.engine.margins(this.signers[0].address)).to.be.deep.eq([
-        utils.parseEther('1000'),
-        utils.parseEther('1000'),
+        parseWei('1000').raw,
+        parseWei('1000').raw,
       ])
     })
 
     it('adds to the margin account of another address when specified', async function () {
       await this.contracts.engineDeposit.deposit(
-        engineAddress,
+        this.contracts.engine.address,
         this.contracts.risky.address,
         this.contracts.stable.address,
         this.contracts.engineDeposit.address,
-        utils.parseEther('1000'),
-        utils.parseEther('1000'),
+        parseWei('1000').raw,
+        parseWei('1000').raw,
       );
 
       expect(await this.contracts.engine.margins(this.contracts.engineDeposit.address)).to.be.deep.eq([
-        utils.parseEther('1000'),
-        utils.parseEther('1000'),
+        parseWei('1000').raw,
+        parseWei('1000').raw,
       ])
     })
 
     it('reverts when the user has insufficient funds', async function () {
       await expect(
         this.contracts.engineDeposit.deposit(
-          engineAddress,
+          this.contracts.engine.address,
           this.contracts.risky.address,
           this.contracts.stable.address,
           this.contracts.engineDeposit.address,
