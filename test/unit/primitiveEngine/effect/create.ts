03ddbe91f7310d72f48c1d52dea07583ae2daca9
Kinrezc <matt.czernik@gmail.com>
2021-06-10 16:07:00 -0400

    fix EngineCreate tests

--- test/unit/primitiveEngine/effect/create.ts
+++ test/unit/primitiveEngine/effect/create.ts
@@ -2,77 +2,37 @@ import { waffle } from 'hardhat'
 import { expect } from 'chai'
 
 import { PERCENTAGE, parseWei } from '../../../shared/Units'
-import { loadContext } from '../../context'
+import loadContext from '../../context'
 
-import {
-  createFragment,
-} from '../fragments'
+import { createFragment } from '../fragments'
 
 const [strike, sigma, time, spot] = [parseWei('1000').raw, 0.85 * PERCENTAGE, 31449600, parseWei('1100').raw]
 
 describe('create', function () {
   before(async function () {
-    await loadContext(
-      waffle.provider,
-      ['factory', 'engineCreate', 'risky', 'stable'],
-      createFragment,
-    );
-  });
+    await loadContext(waffle.provider, ['engineCreate'], createFragment)
+  })
 
   describe('when the parameters are valid', function () {
     it('deploys a new pool', async function () {
-      await this.contracts.engineCreate.create(
-        this.contracts.engine.address,
-        this.contracts.risky.address,
-        this.contracts.stable.address,
-        strike,
-        sigma,
-        time,
-        spot,
-      );
+      await this.contracts.engineCreate.create(strike, sigma, time, spot)
     })
 
     it('emits the Create event', async function () {
-      await expect(
-        this.contracts.engineCreate.create(
-          this.contracts.engine.address,
-          this.contracts.risky.address,
-          this.contracts.stable.address,
+      await expect(this.contracts.engineCreate.create(strike, sigma, time, spot))
+        .to.emit(this.contracts.engine, 'Create')
+        .withArgs(
+          this.contracts.engineCreate.address,
+          '0xbd2b5718c3094a357b195e108feebdacded45272d1086596e5c59b43d017083b',
           strike,
           sigma,
-          time,
-          spot,
+          time
         )
-      ).to.emit(this.contracts.engine, 'Create').withArgs(
-        this.contracts.engineCreate.address,
-        '0x779eb7e81ac17d5ef91f938add39a20c598fbbb64275a3d4df6d6c2f3e03947c',
-        strike,
-        sigma,
-        time
-      )
     })
 
     it('reverts when the pool already exists', async function () {
-      await this.contracts.engineCreate.create(
-        this.contracts.engine.address,
-        this.contracts.risky.address,
-        this.contracts.stable.address,
-        strike,
-        sigma,
-        time,
-        spot,
-      );
-      await expect(
-        this.contracts.engineCreate.create(
-          this.contracts.engine.address,
-          this.contracts.risky.address,
-          this.contracts.stable.address,
-          strike,
-          sigma,
-          time,
-          spot,
-        )
-      ).to.be.revertedWith('Already created')
+      await this.contracts.engineCreate.create(strike, sigma, time, spot)
+      await expect(this.contracts.engineCreate.create(strike, sigma, time, spot)).to.be.revertedWith('Already created')
     })
   })
 })
