09601825c51a4ab77a7c76edefbd510b493373f4
clemlak <clementlakhal@gmail.com>
2021-06-10 11:13:14 +0200

    test: New load context feature

--- test/unit/context.ts
+++ test/unit/context.ts
@@ -1,7 +1,19 @@
 import { createFixtureLoader, Fixture, MockProvider } from 'ethereum-waffle'
 import { Contracts, Mocks } from '../../types'
+import { Contract, Wallet } from 'ethers'
+import hre from 'hardhat'
+import {
+  deployContract,
+  deployMockContract,
+} from 'ethereum-waffle'
 
-export default async function setupContext(
+import {
+  PrimitiveFactory,
+  PrimitiveEngine,
+  Token,
+} from '../../typechain'
+
+export async function setupContext(
   provider: MockProvider,
   fixture: Fixture<any>
 ) {
@@ -10,12 +22,58 @@ export default async function setupContext(
   beforeEach(async function () {
     const loadedFixture = await loadFixture(fixture);
 
+    this.signers = loadedFixture.signers
+
+    this.contracts = {} as Contracts
+
+    this.contracts.factory = loadedFixture.primitiveFactory
+    this.contracts.stable = loadedFixture.stable
+    this.contracts.risky = loadedFixture.risky
+  })
+}
+
+async function deploy(contractName: string, deployer: Wallet): Promise<Contract> {
+  const artifact = await hre.artifacts.readArtifact(contractName);
+  const contract = await deployContract(deployer, artifact);
+  return contract;
+}
+
+export async function loadContext(
+  provider: MockProvider,
+  contracts: string[],
+): Promise<void> {
+  const loadFixture = createFixtureLoader(provider.getWallets(), provider)
+
+  beforeEach(async function () {
+    const loadedFixture = await loadFixture(async function (signers: Wallet[]) {
+      const [deployer] = signers
+      const loadedContracts: Contracts = {} as Contracts
+
+      for (let i = 0; i < contracts.length; i += 1) {
+        const contractName = contracts[i]
+
+        switch (contractName) {
+          case 'factory':
+            loadedContracts.factory = await deploy('PrimitiveFactory', deployer) as PrimitiveFactory
+            break;
+          case 'risky':
+            loadedContracts.risky = await deploy('Token', deployer) as Token
+            break;
+          case 'stable':
+            loadedContracts.stable = await deploy('Token', deployer) as Token
+            break;
+          default:
+            throw new Error('Unknown contract name');
+        }
+      }
+
+      return loadedContracts;
+    })
+
     this.contracts = {} as Contracts
     this.mocks = {} as Mocks
+    this.signers = provider.getWallets()
 
-    this.signers = loadedFixture.signers
-    this.contracts.factory = loadedFixture.primitiveFactory
-    this.mocks.stable = loadedFixture.stable
-    this.mocks.risky = loadedFixture.risky
+    Object.assign(this.contracts, loadedFixture)
   })
 }
