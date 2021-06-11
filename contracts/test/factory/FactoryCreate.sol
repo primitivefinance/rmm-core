8614e2eabe50f6bdcde6abf7e6ab01f65c14a630
Alexangelj <alexangel.j@gmail.com>
2021-06-10 21:08:44 -0700

    feat(factory): Adds Factory test files.

---
+++ contracts/test/factory/FactoryCreate.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.0;
+
+import "../../interfaces/IPrimitiveFactory.sol";
+
+contract FactoryCreate {
+    using SafeERC20 for IERC20;
+
+    address public factory;
+
+    constructor() {}
+
+    function initialize(address factory_) public {
+      factory = factory_;
+    }
+
+    function create(address risky, address riskless) public { 
+      IPrimitiveFactory(factory).create(risky, riskless);
+
+    }
+
+    function name() public view returns (string memory) {
+      return "FactoryCreate";
+    }
+}
+
