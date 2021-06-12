40e1123e81245404b25aaebb69651614e21d2267
Alexangelj <alexangel.j@gmail.com>
2021-06-11 18:41:36 -0700

    feat(black-scholes): Adds unit tests for BlackScholes lib.

--- test/unit/libraries/blackScholes.ts
+++ test/unit/libraries/blackScholes.ts
@@ -1,21 +1,37 @@
 import { waffle } from 'hardhat'
 import { expect } from 'chai'
-import { TestPosition } from '../../../typechain'
-import { parseWei, PERCENTAGE, Wei, fromMantissa, fromInt } from '../../shared/Units'
+import { TestBlackScholes } from '../../../typechain'
+import { parseWei, PERCENTAGE, Wei, fromMantissa, fromInt, YEAR, MANTISSA } from '../../shared/Units'
 import loadContext from '../context'
+import { Calibration } from '../../shared/utilities'
+import { calculateDelta, calculateD1, moneyness } from '../../shared/BlackScholes'
 
-describe('testPosition', function () {
+describe('testBlackScholes', function () {
   before(async function () {
-    await loadContext(waffle.provider, ['testPosition'], async () => {})
+    await loadContext(waffle.provider, ['testBlackScholes'], async () => {})
   })
 
-  describe('position', function () {
-    let position: TestPosition
+  describe('blackScholes', function () {
+    let blackScholes: TestBlackScholes, calibration: Calibration, spot: Wei
 
     beforeEach(async function () {
-      position = this.contracts.testPosition
+      blackScholes = this.contracts.testBlackScholes
+      calibration = { strike: parseWei('1000').raw, sigma: 0.85 * PERCENTAGE, time: YEAR }
+      spot = parseWei('1050')
     })
 
-    it('getProportionalVolatility', async function () {})
+    it('callDelta', async function () {
+      let delta = Math.floor(calculateDelta(calibration, spot) * MANTISSA) / MANTISSA
+      expect(fromMantissa(fromInt(await blackScholes.callDelta(calibration, spot.raw)))).to.be.eq(delta)
+    })
+    it('putDelta', async function () {})
+    it('d1', async function () {
+      let d1 = Math.floor(calculateD1(calibration, spot) * MANTISSA) / MANTISSA
+      expect(fromMantissa(fromInt(await blackScholes.d1(calibration, spot.raw)))).to.be.eq(d1)
+    })
+    it('moneyness', async function () {
+      let simple = Math.floor(moneyness(calibration, spot) * MANTISSA) / MANTISSA
+      expect(fromMantissa(fromInt(await blackScholes.moneyness(calibration, spot.raw)))).to.be.eq(simple)
+    })
   })
 })
