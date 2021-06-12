0edfc7ca1c7ce488643f9e5d590d3f16933d9f7a
Alexangelj <alexangel.j@gmail.com>
2021-06-11 18:58:30 -0700

    feat(cdf): Adds unit tests for CDF lib.

--- test/unit/libraries/cumulativeNormalDistribution.ts
+++ test/unit/libraries/cumulativeNormalDistribution.ts
@@ -1,21 +1,31 @@
 import { waffle } from 'hardhat'
 import { expect } from 'chai'
-import { TestPosition } from '../../../typechain'
-import { parseWei, PERCENTAGE, Wei, fromMantissa, fromInt } from '../../shared/Units'
+import { TestCumulativeNormalDistribution } from '../../../typechain'
+import { fromMantissa, fromInt, parseWei, MANTISSA } from '../../shared/Units'
+import { std_n_cdf, inverse_std_n_cdf } from '../../shared/CumulativeNormalDistribution'
 import loadContext from '../context'
 
-describe('testPosition', function () {
+describe('testCumulativeNormalDistribution', function () {
   before(async function () {
-    await loadContext(waffle.provider, ['testPosition'], async () => {})
+    await loadContext(waffle.provider, ['testCumulativeNormalDistribution'], async () => {})
   })
 
-  describe('position', function () {
-    let position: TestPosition
+  describe('cumulative', function () {
+    let cumulative: TestCumulativeNormalDistribution
 
     beforeEach(async function () {
-      position = this.contracts.testPosition
+      cumulative = this.contracts.testCumulativeNormalDistribution
     })
 
-    it('getProportionalVolatility', async function () {})
+    it('cdf', async function () {
+      let x = 1
+      let cdf = Math.floor(std_n_cdf(x) * MANTISSA) / MANTISSA
+      expect(fromMantissa(fromInt(await cumulative.cdf(x)))).to.be.eq(cdf)
+    })
+    it('icdf', async function () {
+      let x = 0.25
+      let icdf = inverse_std_n_cdf(x)
+      expect(parseFloat((await cumulative.icdf(parseWei(x).raw)).toString()) / Math.pow(2, 64)).to.be.eq(icdf)
+    })
   })
 })
