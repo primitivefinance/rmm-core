519453df59fc221ce4857536f8fc2b36d7f15597
clemlak <clementlakhal@gmail.com>
2021-06-16 19:22:59 +0200

    test: add new deposit tests

--- types/index.d.ts
+++ types/index.d.ts
@@ -21,6 +21,7 @@ export interface Contracts {
   engineAllocate: ContractTypes.EngineAllocate
   engineRemove: ContractTypes.EngineRemove
   engineLend: ContractTypes.EngineLend
+  badEngineDeposit: ContractTypes.BadEngineDeposit
   factoryCreate: ContractTypes.FactoryCreate
   factoryDeploy: ContractTypes.FactoryDeploy
   testReserve: ContractTypes.TestReserve
@@ -63,3 +64,4 @@ type ContractName =
   | 'testCumulativeNormalDistribution'
   | 'engineRemove'
   | 'engineLend'
+  | 'badEngineDeposit'
