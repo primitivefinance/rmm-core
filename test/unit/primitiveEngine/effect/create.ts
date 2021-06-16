a98790856c658ce0af686ecd7e35cebca62c8418
clemlak <clementlakhal@gmail.com>
2021-06-16 15:45:32 +0200

    test: add new create tests

--- test/unit/primitiveEngine/effect/create.ts
+++ test/unit/primitiveEngine/effect/create.ts
@@ -11,7 +11,7 @@ const empty: BytesLike = constants.HashZero
 
 describe('create', function () {
   before(async function () {
-    await loadContext(waffle.provider, ['engineCreate'], createFragment)
+    await loadContext(waffle.provider, ['engineCreate', 'testPosition'], createFragment)
   })
 
   describe('when the parameters are valid', function () {
@@ -31,16 +31,36 @@ describe('create', function () {
       await this.contracts.engineCreate.create(strike, sigma, time, spot, parseWei('1').raw, empty)
 
       const poolId = await this.contracts.engine.getPoolId(strike, sigma, time)
+      const pos = await this.contracts.engineCreate.fetch(poolId)
 
-      const position = await await this.contracts.engine.positions(poolId)
-      console.log(position)
+      expect(pos.liquidity).to.equal(parseWei('1').sub('1000').raw)
+    })
+
+    it('updates the reserves of the engine', async function () {
+      const tx = await this.contracts.engineCreate.create(strike, sigma, time, spot, parseWei('1').raw, empty)
+      const receipt = await tx.wait()
+      const { timestamp } = await waffle.provider.getBlock(receipt.blockNumber)
+
+      const poolId = await this.contracts.engine.getPoolId(strike, sigma, time)
+
+      const reserve = await this.contracts.engine.reserves(poolId)
+      console.log(reserve)
+
+      // TODO: Check RX1 and RY2
+
+      expect(reserve.liquidity).to.equal(parseWei('1').raw)
+      expect(reserve.float).to.equal(0)
+      expect(reserve.debt).to.equal(0)
+      expect(reserve.cumulativeLiquidity).to.equal(0)
+      expect(reserve.cumulativeRisky).to.equal(0)
+      expect(reserve.cumulativeStable).to.equal(0)
+      expect(reserve.blockTimestamp).to.equal(timestamp)
     })
 
     it('increases the engine contract balances', async function () {
       await this.contracts.engineCreate.create(strike, sigma, time, spot, parseWei('1').raw, empty)
 
       expect(await this.contracts.risky.balanceOf(this.contracts.engine.address)).to.not.equal(0)
-
       expect(await this.contracts.stable.balanceOf(this.contracts.engine.address)).to.not.equal(0)
     })
 
