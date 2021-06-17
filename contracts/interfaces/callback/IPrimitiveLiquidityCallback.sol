55954260ad18e50370fc92f27854c12c199320a1
Alexangelj <alexangel.j@gmail.com>
2021-06-17 14:49:15 -0700

    Adds natspec to callback interfaces.

--- contracts/interfaces/callback/IPrimitiveLiquidityCallback.sol
+++ contracts/interfaces/callback/IPrimitiveLiquidityCallback.sol
@@ -1,15 +1,26 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.0;
 
+/// @title  Primitive Liquidity Callbacks
+/// @author Primitive
+
 interface IPrimitiveLiquidityCallback {
+    /// @notice Triggered when providing liquidity to an Engine
+    /// @param  delRisky     Amount of risky tokens required to provide to risky reserve
+    /// @param  delStable    Amount of stable tokens required to provide to stable reserve
+    /// @param  data         Calldata passed on allocate function call
     function allocateCallback(
-        uint256 deltaX,
+        uint256 delRisky,
         uint256 deltaY,
         bytes calldata data
     ) external;
 
+    /// @notice Triggered when removing liquidity from an Engine
+    /// @param  delRisky     Amount of risky tokens being removed from risky reserve
+    /// @param  delStable    Amount of stable tokens being removed from stable reserve
+    /// @param  data         Calldata passed on remove function call
     function removeCallback(
-        uint256 deltaX,
+        uint256 delRisky,
         uint256 deltaY,
         bytes calldata data
     ) external;
--- contracts/interfaces/callback/IPrimitiveMarginCallback.sol
+++ contracts/interfaces/callback/IPrimitiveMarginCallback.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.0;
 
+/// @title  Primitive Margin Callback
+/// @author Primitive
+
 interface IPrimitiveMarginCallback {
+    /// @notice Triggered when depositing tokens to an Engine
+    /// @param  delRisky     Amount of risky tokens required to deposit to risky margin balance
+    /// @param  delStable    Amount of stable tokens required to deposit to stable margin balance
+    /// @param  data         Calldata passed on deposit function call
     function depositCallback(
-        uint256 deltaX,
-        uint256 deltaY,
+        uint256 delRisky,
+        uint256 delStable,
         bytes calldata data
     ) external;
 }
