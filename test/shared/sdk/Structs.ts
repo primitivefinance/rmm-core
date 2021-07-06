6f9eddc64a5212b3a28df442a85fe9a88c223edd
Alex Angel <alexangel.j@gmail.com>
2021-07-06 08:53:15 -0700

    fix(struct.ts): natspec

--- test/shared/sdk/Structs.ts
+++ test/shared/sdk/Structs.ts
@@ -1,12 +1,19 @@
 /// SDK Imports
 import { Wei, Percentage, Time } from './Units'
 
+/**
+ * @notice Defines a pool's parameters in the Engine
+ */
 export interface Calibration {
   strike: Wei
   sigma: Percentage
   maturity: Time
   lastTimestamp: Time
 }
+
+/**
+ * @notice A Pool's global balance state in the Engine
+ */
 export interface Reserve {
   reserveRisky: Wei
   reserveStable: Wei
@@ -14,10 +21,18 @@ export interface Reserve {
   float: Wei
   debt: Wei
 }
+
+/**
+ * @notice Global user internal balance in the Engine
+ */
 export interface Margin {
   balanceRisky: Wei
   balanceStable: Wei
 }
+
+/**
+ * @notice Individual position data in a Pool
+ */
 export interface Position {
   liquidity: Wei
   float: Wei
