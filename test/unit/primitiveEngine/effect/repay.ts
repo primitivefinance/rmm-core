c90fe7fcfe4cd42bb96c07e80739bb349b5b5b7a
alex <alexangel.j@gmail.com>
2021-08-12 16:49:58 -0700

    test(repay): adds repay tests

--- test/unit/primitiveEngine/effect/repay.ts
+++ test/unit/primitiveEngine/effect/repay.ts
@@ -1,13 +1,14 @@
 import expect from '../../../shared/expect'
 import { waffle } from 'hardhat'
-import { parseWei } from 'web3-units'
+import { parseWei, Time, toBN } from 'web3-units'
 import { constants, Wallet } from 'ethers'
 
 import loadContext, { DEFAULT_CONFIG as config } from '../../context'
 import { Contracts } from '../../../../types'
 import { computePoolId } from '../../../shared/utils'
+import { Calibration } from '../../../shared'
 
-const { strike, sigma, maturity, lastTimestamp, spot, delta } = config
+const { strike, sigma, maturity, delta } = config
 const { HashZero } = constants
 
 export async function beforeEachRepay(signers: Wallet[], contracts: Contracts): Promise<void> {
@@ -24,7 +25,7 @@ describe('repay', function () {
   before(async function () {
     loadContext(
       waffle.provider,
-      ['engineCreate', 'engineDeposit', 'engineAllocate', 'engineSupply', 'engineRepay'],
+      ['engineCreate', 'engineDeposit', 'engineAllocate', 'engineSupply', 'engineRepay', 'engineBorrow'],
       beforeEachRepay
     )
   })
@@ -46,10 +47,38 @@ describe('repay', function () {
       expect(position.debt).to.equal(0)
     })
 
-    it('allocates to the reserve', async function () {
+    it('res.allocate: increases risky reserve', async function () {
+      const res = await this.contracts.engine.reserves(poolId)
+      const delRisky = one.mul(res.reserveRisky).div(res.liquidity)
+      await expect(
+        this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+      ).to.increaseReserveRisky(this.contracts.engine, poolId, delRisky.raw)
+    })
+
+    it('res.allocate: increases stable reserve', async function () {
+      const res = await this.contracts.engine.reserves(poolId)
+      const delStable = one.mul(res.reserveStable).div(res.liquidity)
+      await expect(
+        this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+      ).to.increaseReserveStable(this.contracts.engine, poolId, delStable.raw)
+    })
+
+    it('res.allocate: increases reserve liquidity', async function () {
+      await expect(
+        this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+      ).to.increaseReserveLiquidity(this.contracts.engine, poolId, one.raw)
+    })
+
+    it('res.allocate: updates reserve blocktimestamp', async function () {
+      await expect(
+        this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+      ).to.updateReserveBlockTimestamp(this.contracts.engine, poolId, +(await this.contracts.engine.time()))
+    })
+
+    it('allocates to the reserve and updates all its values', async function () {
       const oldReserve = await this.contracts.engine.reserves(poolId)
-      const delRisky = parseWei('1').mul(oldReserve.reserveRisky).div(oldReserve.liquidity)
-      const delStable = parseWei('1').mul(oldReserve.reserveStable).div(oldReserve.liquidity)
+      const delRisky = one.mul(oldReserve.reserveRisky).div(oldReserve.liquidity)
+      const delStable = one.mul(oldReserve.reserveStable).div(oldReserve.liquidity)
 
       await expect(
         this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
@@ -60,6 +89,18 @@ describe('repay', function () {
       expect(newReserve.reserveRisky).to.equal(oldReserve.reserveRisky.add(delRisky.raw))
       expect(newReserve.reserveStable).to.equal(oldReserve.reserveStable.add(delStable.raw))
       expect(newReserve.liquidity).to.equal(oldReserve.liquidity.add(one.raw))
+    })
+
+    it('res.repayFloat: decreases reserve debt', async function () {
+      await expect(
+        this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+      ).to.decreaseReserveDebt(this.contracts.engine, poolId, one.raw)
+    })
+
+    it('res.repayFloat: increases reserve float', async function () {
+      await expect(
+        this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+      ).to.increaseReserveFloat(this.contracts.engine, poolId, one.raw)
     })
 
     it('reduces the debt and increases the float of the reserve', async function () {
@@ -87,13 +128,13 @@ describe('repay', function () {
     })
 
     describe('when from margin', function () {
-      it('reduces the delStable margin of the caller', async function () {
+      it('reduces stable in margin by delStable, increases risky in margin by premium', async function () {
         await this.contracts.engineDeposit.deposit(this.contracts.engineRepay.address, 0, parseWei('400').raw, HashZero)
 
         const oldReserve = await this.contracts.engine.reserves(poolId)
-        const delRisky = parseWei('1').mul(oldReserve.reserveRisky).div(oldReserve.liquidity)
-        const delStable = parseWei('1').mul(oldReserve.reserveStable).div(oldReserve.liquidity)
-        const premium = parseWei('1').sub(delRisky)
+        const delRisky = one.mul(oldReserve.reserveRisky).div(oldReserve.liquidity)
+        const delStable = one.mul(oldReserve.reserveStable).div(oldReserve.liquidity)
+        const premium = one.sub(delRisky)
         const margin = await this.contracts.engine.margins(this.contracts.engineRepay.address)
 
         await expect(
@@ -108,26 +149,30 @@ describe('repay', function () {
     })
 
     describe('when from external', function () {
-      it("transfers the premium to the caller's wallet", async function () {
+      it('transfers the premium to the caller of repay', async function () {
         const previousRiskyBalance = await this.contracts.risky.balanceOf(this.signers[0].address)
 
         const oldReserve = await this.contracts.engine.reserves(poolId)
-        const delRisky = parseWei('1').mul(oldReserve.reserveRisky).div(oldReserve.liquidity)
-        const premium = parseWei('1').sub(delRisky)
+        const delRisky = one.mul(oldReserve.reserveRisky).div(oldReserve.liquidity)
+        const premium = one.sub(delRisky)
 
-        await this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+        await expect(() =>
+          this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+        ).to.changeTokenBalances(this.contracts.risky, [this.signers[0]], [premium.raw])
 
         expect(await this.contracts.risky.balanceOf(this.signers[0].address)).to.equal(previousRiskyBalance.add(premium.raw))
       })
 
-      it('transfers the stable from the callers wallet to the engine', async function () {
+      it('transfers the stable from the caller to the engine', async function () {
         const signerPreviousStableBalance = await this.contracts.stable.balanceOf(this.signers[0].address)
         const enginePreviousStableBalance = await this.contracts.stable.balanceOf(this.contracts.engine.address)
 
         const oldReserve = await this.contracts.engine.reserves(poolId)
-        const delStable = parseWei('1').mul(oldReserve.reserveStable).div(oldReserve.liquidity)
+        const delStable = one.mul(oldReserve.reserveStable).div(oldReserve.liquidity)
 
-        await this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+        await expect(() =>
+          this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, false, HashZero)
+        ).to.changeTokenBalances(this.contracts.stable, [this.contracts.engine], [delStable.raw])
 
         expect(await this.contracts.stable.balanceOf(this.signers[0].address)).to.equal(
           signerPreviousStableBalance.sub(delStable.raw)
@@ -136,6 +181,62 @@ describe('repay', function () {
         expect(await this.contracts.stable.balanceOf(this.contracts.engine.address)).to.equal(
           enginePreviousStableBalance.add(delStable.raw)
         )
+      })
+    })
+
+    describe('when expired', function () {
+      let expiredPoolId: string
+      beforeEach(async function () {
+        const fig = new Calibration(10, 1, Time.YearInSeconds, Time.YearInSeconds + 1, 10)
+        await this.contracts.engineCreate.create(
+          fig.strike.raw,
+          fig.sigma.raw,
+          fig.maturity.raw,
+          parseWei(fig.delta).raw,
+          one.raw,
+          HashZero
+        )
+        expiredPoolId = computePoolId(this.contracts.engine.address, fig.maturity.raw, fig.sigma.raw, fig.strike.raw)
+        await this.contracts.engine.advanceTime(Time.YearInSeconds + 1)
+        // give liquidity to engineSupply contract
+        await this.contracts.engineAllocate.allocateFromExternal(
+          expiredPoolId,
+          this.contracts.engineSupply.address,
+          parseWei('100').raw,
+          HashZero
+        )
+        // have the engineSupply contract supply the lp shares
+        await this.contracts.engineSupply.supply(expiredPoolId, parseWei('100').raw)
+        // have the engineBorrow borrow the lp shares
+        await this.contracts.engineBorrow.borrow(
+          expiredPoolId,
+          this.contracts.engineBorrow.address,
+          parseWei('1').raw,
+          HashZero
+        )
+      })
+
+      it('repay engineBorrow borrow position, called by engineRepay: reduces stable in margin for engineRepay', async function () {
+        await this.contracts.engineDeposit.deposit(this.contracts.engineRepay.address, 0, parseWei('400').raw, HashZero)
+
+        const oldReserve = await this.contracts.engine.reserves(expiredPoolId)
+        const delStable = one.mul(oldReserve.reserveStable).div(oldReserve.liquidity)
+
+        await expect(
+          this.contracts.engineRepay.repay(expiredPoolId, this.contracts.engineBorrow.address, one.raw, true, HashZero)
+        ).to.decreaseMargin(this.contracts.engine, this.contracts.engineRepay.address, toBN(0), delStable.raw)
+      })
+
+      it('repay engineBorrow borrow position, called by engineRepay: increases risky in margin for engineBorrow', async function () {
+        await this.contracts.engineDeposit.deposit(this.contracts.engineRepay.address, 0, parseWei('400').raw, HashZero)
+
+        const oldReserve = await this.contracts.engine.reserves(expiredPoolId)
+        const delRisky = one.mul(oldReserve.reserveRisky).div(oldReserve.liquidity)
+        const premium = one.sub(delRisky)
+
+        await expect(
+          this.contracts.engineRepay.repay(expiredPoolId, this.contracts.engineBorrow.address, one.raw, true, HashZero)
+        ).to.increaseMargin(this.contracts.engine, this.contracts.engineBorrow.address, premium.raw, toBN(0))
       })
     })
   })
@@ -166,8 +267,10 @@ describe('repay', function () {
         await expect(this.contracts.engineRepay.repay(poolId, this.contracts.engineRepay.address, one.raw, true, HashZero))
           .to.be.reverted
       })
+    })
 
-      it('reverts if the stable is not sent to repay', async function () {
+    describe('when from external', function () {
+      it('reverts if stable was not paid in callback', async function () {
         await expect(
           this.contracts.engineRepay.repayWithoutRepaying(
             poolId,
