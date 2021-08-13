61f7c005556bb8e7e23ee70f557cbbb7f1d48a2f
alex <alexangel.j@gmail.com>
2021-08-12 20:35:31 -0700

    style(Reserve): natspec on reserve lib

--- contracts/libraries/Reserve.sol
+++ contracts/libraries/Reserve.sol
@@ -1,29 +1,28 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.6;
 
-/// @notice  Engine Reserves
+/// @notice  Reserves Library
 /// @author  Primitive
-/// @dev     Data structure library for an Engine's Reserves.
+/// @dev     Data structure library for an Engine's Reserves
 
 import "./SafeCast.sol";
 
 library Reserve {
     using SafeCast for uint256;
 
-    // An Engine has two reserves of RISKY and STABLE assets, total liquidity, and cumulative balances.
     struct Data {
-        uint128 reserveRisky; // reserve for the risky asset
-        uint128 reserveStable; // reserve for the stable asset
+        uint128 reserveRisky; // reserve of the risky asset
+        uint128 reserveStable; // reserve of the stable asset
         uint128 liquidity; // total supply of liquidity
-        uint128 float; // liquidity available for borrowing
+        uint128 float; // liquidity supplied to be borrowed
         uint128 debt; // liquidity unavailable because it was borrowed
-        uint32 blockTimestamp; // last timestamp for updated cumulative reserves
-        uint256 cumulativeRisky;
-        uint256 cumulativeStable;
-        uint256 cumulativeLiquidity;
+        uint32 blockTimestamp; // last timestamp of updated cumulative reserves
+        uint256 cumulativeRisky; // cumulative sum of risky reserves
+        uint256 cumulativeStable; // cumulative sum of stable reserves
+        uint256 cumulativeLiquidity; // cumulative sum of total liquidity supply
     }
 
-    /// @notice Adds to the cumulative reserves
+    /// @notice                 Adds to the cumulative reserves
     /// @param  res             Reserve storage to update
     /// @param  blockTimestamp  Checkpoint timestamp of update
     function update(Data storage res, uint32 blockTimestamp) internal {
@@ -39,11 +38,11 @@ library Reserve {
         res.blockTimestamp = blockTimestamp;
     }
 
-    /// @notice Increases one reserve value and decreases the other by different amounts
+    /// @notice                 Increases one reserve value and decreases the other
     /// @param  reserve         Reserve storage to update
     /// @param  riskyForStable  Direction of swap
-    /// @param  deltaIn         Amount of tokens paid
-    /// @param  deltaOut        Amount of tokens sent out
+    /// @param  deltaIn         Amount of tokens paid, increases one reserve by
+    /// @param  deltaOut        Amount of tokens sent out, decreases the other reserve by
     /// @param  blockTimestamp  Checkpoint timestamp of swap
     function swap(
         Data storage reserve,
@@ -62,11 +61,11 @@ library Reserve {
         update(reserve, blockTimestamp);
     }
 
-    /// @notice Add to both reserves and total supply of liquidity
+    /// @notice                 Add to both reserves and total supply of liquidity
     /// @param  reserve         Reserve storage to manipulate
     /// @param  delRisky        Amount of risky tokens to add to the reserve
     /// @param  delStable       Amount of stable tokens to add to the reserve
-    /// @param  delLiquidity    Amount of liquidity minted with the provided tokens
+    /// @param  delLiquidity    Amount of liquidity created with the provided tokens
     /// @param  blockTimestamp  Checkpoint timestamp of allocation
     function allocate(
         Data storage reserve,
@@ -81,7 +80,7 @@ library Reserve {
         update(reserve, blockTimestamp);
     }
 
-    /// @notice Remove from both reserves and total supply of liquidity
+    /// @notice                 Remove from both reserves and total supply of liquidity
     /// @param  reserve         Reserve storage to manipulate
     /// @param  delRisky        Amount of risky tokens to remove to the reserve
     /// @param  delStable       Amount of stable tokens to remove to the reserve
@@ -100,31 +99,31 @@ library Reserve {
         update(reserve, blockTimestamp);
     }
 
-    /// @notice Increases available float to borrow, called when borrowing
-    /// @param reserve      Reserve storage to manipulate
-    /// @param delLiquidity Amount of liquidity to add to float
+    /// @notice                 Increases available float to borrow, called when supplying
+    /// @param reserve          Reserve storage to manipulate
+    /// @param delLiquidity     Amount of liquidity to add to float
     function addFloat(Data storage reserve, uint256 delLiquidity) internal {
         reserve.float += delLiquidity.toUint128();
     }
 
-    /// @notice Reduces available float, taking liquidity off the market, called when claiming
-    /// @param reserve      Reserve storage to manipulate
-    /// @param delLiquidity Amount of liquidity to remove from float
+    /// @notice                 Reduces available float, called when claiming
+    /// @param reserve          Reserve storage to manipulate
+    /// @param delLiquidity     Amount of liquidity to remove from float
     function removeFloat(Data storage reserve, uint256 delLiquidity) internal {
         reserve.float -= delLiquidity.toUint128();
     }
 
-    /// @notice Reduces float and increases debt of the global reserve, called when borrowing
-    /// @param reserve      Reserve storage to manipulate
-    /// @param delLiquidity Amount of liquidity to remove from float and add to debt
+    /// @notice                 Reduces float and increases debt of the global reserve, called when borrowing
+    /// @param reserve          Reserve storage to manipulate
+    /// @param delLiquidity     Amount of liquidity to remove from float and add to debt
     function borrowFloat(Data storage reserve, uint256 delLiquidity) internal {
         reserve.float -= delLiquidity.toUint128();
         reserve.debt += delLiquidity.toUint128();
     }
 
-    /// @notice Increases float and reduces debt of the global reserve, called when repaying a borrow
-    /// @param reserve      Reserve storage to manipulate
-    /// @param delLiquidity Amount of liquidity to add to float and remove from debt
+    /// @notice                 Increases float and reduces debt of the global reserve, called when repaying a borrow
+    /// @param reserve          Reserve storage to manipulate
+    /// @param delLiquidity     Amount of liquidity to add to float and remove from debt
     function repayFloat(Data storage reserve, uint256 delLiquidity) internal {
         reserve.float += delLiquidity.toUint128();
         reserve.debt -= delLiquidity.toUint128();
