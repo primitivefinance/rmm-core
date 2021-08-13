4cf7cf5e314739b9cc36c941f7e70f152ead682b
alex <alexangel.j@gmail.com>
2021-08-12 20:09:29 -0700

    style(CDF): natspec on CDF lib

--- contracts/libraries/CumulativeNormalDistribution.sol
+++ contracts/libraries/CumulativeNormalDistribution.sol
@@ -9,18 +9,14 @@ import "./ABDKMath64x64.sol";
 library CumulativeNormalDistribution {
     using ABDKMath64x64 for *;
 
-    /// @notice Returns the Normal Standard Cumulative Distribution Function of `x`
+    /// @notice Returns the Standard Normal Cumulative Distribution Function of `x`
     function getCDF(int128 x) internal pure returns (int128) {
         // where p = 0.3275911,
         // a1 = 0.254829592, a2 = −0.284496736, a3 = 1.421413741, a4 = −1.453152027, a5 = 1.061405429
         int128 p = 0x53dd02a4f5ee2e46;
         int128 one = uint256(1).fromUInt();
         int128 two = uint256(2).fromUInt();
-        // int128 a1 = 0x413c831bb169f874;
-        // int128 a2 = -0x48d4c730f051a5fe;
         int128 a3 = 0x16a09e667f3bcc908;
-        // int128 a4 = -0x17401c57014c38f14;
-        // int128 a5 = 0x10fb844255a12d72e;
         int128 z = x.div(a3);
         int128 t = one.div(one.add(p.mul(z.abs())));
         int128 erf = getErrorFunction(z, t);
@@ -55,6 +51,7 @@ library CumulativeNormalDistribution {
 
     /// @notice  Returns the inverse CDF, or quantile function of `p`.
     /// @dev     Source: https://arxiv.org/pdf/1002.0567.pdf
+    /// @return  fcentral(p) = q * (a2 + (a1r + a0) / (r^2 + b1r +b0))
     function getInverseCDF(int128 p) internal pure returns (int128) {
         int128 half = 0x8000000000001060; // 0.5
         int128 q = p.sub(half);
@@ -64,7 +61,6 @@ library CumulativeNormalDistribution {
         int128 a2 = 0x15D71F57212414CA0; // 1.365020123
         int128 b0 = 0x21D0A04B0E9BA0F0; // 0.132089632
         int128 b1 = -0xC2BF5D74C7247680; // -0.760732499
-        // fcentral(p) = q * (a2 + (a1r + a0) / (r^2 + b1r +b0))
         int128 result = q.mul(a2.add((a1.mul(r).add(a0)).div((r.pow(2).add(b1.mul(r)).add(b0)))));
         return result;
     }
