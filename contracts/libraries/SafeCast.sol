ad9a9d421800f012ee92e795436e5e747a268110
alex <alexangel.j@gmail.com>
2021-08-12 20:26:30 -0700

    style(SafeCast): natspec on safecast lib

--- contracts/libraries/SafeCast.sol
+++ contracts/libraries/SafeCast.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.6;
 
+/// @notice Safely cast between uint256 and uint128 or uint64
+
 library SafeCast {
+    /// @notice reverts if x > type(uint128).max
     function toUint128(uint256 x) internal pure returns (uint128 z) {
         require((z = uint128(x)) == x);
     }
 
+    /// @notice reverts if x > type(uint64).max
     function toUint64(uint256 x) internal pure returns (uint128 z) {
         require((z = uint64(x)) == x);
     }
