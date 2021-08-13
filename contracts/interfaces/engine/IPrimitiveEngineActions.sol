9d60e51fc10959524d518295bf3ded3db283150d
alex <alexangel.j@gmail.com>
2021-08-12 19:45:10 -0700

    fix(IActions): natspec in IActions

--- contracts/interfaces/engine/IPrimitiveEngineActions.sol
+++ contracts/interfaces/engine/IPrimitiveEngineActions.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.6;
 
-/// @title  The action functions for the Primitive Engine contract
+/// @title  Action functions for the Primitive Engine contract
 /// @author Primitive
 
 interface IPrimitiveEngineActions {
-    /// @notice         Initializes a curve with parameters in the `settings` storage mapping in the Engine
-    /// @param  strike  Strike price of the option to calibrate to
-    /// @param  sigma   Volatility of the option to calibrate to
-    /// @param  maturity Maturity timestamp of the option
-    /// @param  delta   Call option delta, greek to represent the change in option value wrt to a 1% change in underlying value
-    /// @param  delLiquidity Amount of liquidity to mint
-    /// @param  data    Arbitrary data that is passed to the createCallback function
-    /// @return poolId  Keccak256 hash of the parameters strike, sigma, and maturity, use to identify this option
-    /// delRisky        Amount of risky tokens provided to reserves
-    /// delStable       Amount of stable tokens provided to reserves
+    /// @notice             Initializes a curve with parameters in the `settings` storage mapping in the Engine
+    /// @param  strike      Strike price of the option to calibrate to
+    /// @param  sigma       Volatility of the option to calibrate to
+    /// @param  maturity    Maturity timestamp of the option
+    /// @param  delta       Call option delta, change in option value wrt to a 1% change in underlying value
+    /// @param  delLiquidity Amount of liquidity to allocate to the curve
+    /// @param  data        Arbitrary data that is passed to the createCallback function
+    /// @return poolId      Keccak256 hash of the parameters (engine, strike, sigma, and maturity)
+    /// delRisky            Amount of risky tokens provided to reserves
+    /// delStable           Amount of stable tokens provided to reserves
     function create(
         uint256 strike,
         uint64 sigma,
@@ -31,7 +31,7 @@ interface IPrimitiveEngineActions {
         );
 
     // ===== Margin ====
-    /// @notice Adds risky and/or stable tokens to a `msg.sender`'s internal balance account
+    /// @notice             Adds risky and/or stable tokens to a `recipient`'s internal balance account
     /// @param  recipient   Recipient margin account of the deposited tokens
     /// @param  delRisky    Amount of risky tokens to deposit
     /// @param  delStable   Amount of stable tokens to deposit
@@ -43,7 +43,7 @@ interface IPrimitiveEngineActions {
         bytes calldata data
     ) external;
 
-    /// @notice Removes risky and/or stable tokens from a `msg.sender`'s internal balance account
+    /// @notice             Removes risky and/or stable tokens from a `msg.sender`'s internal balance account
     /// @param  recipient   Address that tokens are transferred to
     /// @param  delRisky    Amount of risky tokens to withdraw
     /// @param  delStable   Amount of stable tokens to withdraw
@@ -54,7 +54,7 @@ interface IPrimitiveEngineActions {
     ) external;
 
     // ===== Liquidity =====
-    /// @notice Allocates risky and stable tokens to a specific curve with `poolId`
+    /// @notice             Allocates risky and stable tokens to a specific curve with `poolId`
     /// @param  poolId      Keccak hash of the option parameters of a curve to interact with
     /// @param  recipient   Address to give the allocated position to
     /// @param  delLiquidity  Quantity of liquidity units to get allocated
@@ -70,18 +70,18 @@ interface IPrimitiveEngineActions {
         bytes calldata data
     ) external returns (uint256, uint256);
 
-    /// @notice Unallocates risky and stable tokens from a specific curve with `poolId`
-    /// @param  poolId          Keccak hash of the option parameters of a curve to interact with
-    /// @param  delLiquidity    Amount of liquidity to burn to release tokens
-    /// @return delRisky        Amount of risky tokens received from the burned liquidity
-    /// delStable               Amount of stable tokens received from the burned liquidity
+    /// @notice             Unallocates risky and stable tokens from a specific curve with `poolId`
+    /// @param  poolId      Keccak hash of the option parameters of a curve to interact with
+    /// @param  delLiquidity Amount of liquidity to burn to release tokens
+    /// @return delRisky    Amount of risky tokens received from the burned liquidity
+    /// delStable           Amount of stable tokens received from the burned liquidity
     function remove(bytes32 poolId, uint256 delLiquidity) external returns (uint256 delRisky, uint256 delStable);
 
     // ===== Swaps =====
-    /// @notice Swaps risky or stable tokens
+    /// @notice             Swaps risky or stable tokens
     /// @param  poolId      Keccak hash of the option parameters of a curve to interact with
     /// @param  riskyForStable Whether to do a risky to stable token swap, or stable to risky swap
-    /// @param  deltaIn     Amount of tokens to swap
+    /// @param  deltaIn     Amount of tokens to swap in
     /// @param  fromMargin  Whether the `msg.sender` uses their margin balance, or must send tokens
     /// @param  data        Arbitrary data that is passed to the swapCallback function
     /// @return deltaOut    Amount of either stable or risky tokens that were sent out of this contract as payment
@@ -94,24 +94,27 @@ interface IPrimitiveEngineActions {
     ) external returns (uint256 deltaOut);
 
     // ===== Convexity =====
-    /// @notice Increases the `msg.sender`'s position's float value. Supplies liquidity to be borrowed.
-    /// @param  poolId          Keccak hash of the option parameters of a curve to interact with
-    /// @param  delLiquidity    Amount of liquidity to add to the float
+    /// @notice             Supplies liquidity to be borrowed
+    /// @dev                Increases the `msg.sender`'s position's float value.
+    /// @param  poolId      Keccak hash of the option parameters of a curve to interact with
+    /// @param  delLiquidity Amount of liquidity to add to the float
     function supply(bytes32 poolId, uint256 delLiquidity) external;
 
-    /// @notice Reduces the `msg.sender`'s position's float value. Removes supplied liquidity.
-    /// @param  poolId          Keccak hash of the option parameters of a curve to interact with
-    /// @param  delLiquidity    Amount of liquidity to remove from the float
+    /// @notice             Removes supplied liquidity.
+    /// @dev                Reduces the `msg.sender`'s position's float value.
+    /// @param  poolId      Keccak hash of the option parameters of a curve to interact with
+    /// @param  delLiquidity Amount of liquidity to remove from the float
     function claim(bytes32 poolId, uint256 delLiquidity) external;
 
-    /// @notice Increases the `msg.sender`'s position's liquidity value and also adds the same to the debt value.
-    /// @param  poolId          Keccak hash of the option parameters of a curve to interact with
-    /// @param  delLiquidity    Amount of liquidity to borrow and add as debt
-    /// @param  fromMargin      Use margin risky balance to pay premium?
-    /// @param  data            Arbitrary data that is passed to the borrowCallback function
-    /// @return delRisky        Amount of risky tokens removed from liquidity borrowed
-    /// delStable               Amount of stable tokens removed from liquidity borrowed
-    /// premium                 Price paid to open position
+    /// @notice             Borrows liquidity and removes it, adding a debt
+    /// @dev                Increases the `msg.sender`'s position's liquidity value and adds the same to the debt
+    /// @param  poolId      Keccak hash of the option parameters of a curve to interact with
+    /// @param  delLiquidity Amount of liquidity to borrow and add as debt
+    /// @param  fromMargin  Use margin risky balance to pay premium?
+    /// @param  data        Arbitrary data that is passed to the borrowCallback function
+    /// @return delRisky    Amount of risky tokens removed from liquidity borrowed
+    /// delStable           Amount of stable tokens removed from liquidity borrowed
+    /// premium             Price paid to open position
     function borrow(
         bytes32 poolId,
         uint256 delLiquidity,
@@ -125,15 +128,16 @@ interface IPrimitiveEngineActions {
             uint256 premium
         );
 
-    /// @notice Reduces the `msg.sender`'s position's liquidity value and also reduces the same to the debt value.
-    /// @param  poolId          Keccak hash of the option parameters of a curve to interact with
-    /// @param  recipient       Position recipient to grant the borrowed liquidity shares
-    /// @param  delLiquidity    Amount of liquidity to borrow and add as debt
-    /// @param  fromMargin      Whether the `msg.sender` uses their margin balance, or must send tokens
-    /// @param  data            Arbitrary data that is passed to the repayCallback function
-    /// @return delRisky        Amount of risky tokens allocated as liquidity to pay debt
-    /// delStable               Amount of stable tokens allocated as liquidity to pay debt
-    /// premium                 Price paid to the `recipient`'s margin account
+    /// @notice             Pays back liquidity share debt by allocating liquidity
+    /// @dev                Reduces the `msg.sender`'s position's liquidity value and reduces the same to the debt value
+    /// @param  poolId      Keccak hash of the option parameters of a curve to interact with
+    /// @param  recipient   Position recipient to grant the borrowed liquidity shares
+    /// @param  delLiquidity Amount of liquidity to borrow and add as debt
+    /// @param  fromMargin  Whether the `msg.sender` uses their margin balance, or must send tokens
+    /// @param  data        Arbitrary data that is passed to the repayCallback function
+    /// @return delRisky    Amount of risky tokens allocated as liquidity to pay debt
+    /// delStable           Amount of stable tokens allocated as liquidity to pay debt
+    /// premium             Price paid to the `recipient`'s margin account
     function repay(
         bytes32 poolId,
         address recipient,
