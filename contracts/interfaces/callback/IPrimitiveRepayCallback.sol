33f876541574030d53aa2936b4883cd3446e413a
alex <alexangel.j@gmail.com>
2021-08-24 16:10:28 -0700

    refactor(repay): improves readability, removes code

--- contracts/interfaces/callback/IPrimitiveRepayCallback.sol
+++ contracts/interfaces/callback/IPrimitiveRepayCallback.sol
@@ -5,13 +5,13 @@ pragma solidity 0.8.6;
 /// @author Primitive
 
 interface IPrimitiveRepayCallback {
-    /// @notice                 Triggered when repaying liquidity to an Engine
-    /// @param  riskyDeficit    Amount of risky tokens required to be paid to the Engine
-    /// @param  stableDeficit   Amount of stable tokens required to be paid to the Engine
-    /// @param  data            Calldata passed on repay function call
+    /// @notice             Triggered when repaying liquidity to an Engine
+    /// @param  riskyIn     Amount of risky tokens required to be paid to the Engine
+    /// @param  stableIn    Amount of stable tokens required to be paid to the Engine
+    /// @param  data        Calldata passed on repay function call
     function repayCallback(
-        uint256 riskyDeficit,
-        uint256 stableDeficit,
+        uint256 riskyIn,
+        uint256 stableIn,
         bytes calldata data
     ) external;
 }
