09863673dbd0b35d2e143cf994753d134cec6ac9
alex <alexangel.j@gmail.com>
2021-09-02 21:20:38 -0700

    test(cdf-icdf): adds negative value checks to cdf and icdf

--- test/unit/libraries/cumulativeNormalDistribution.ts
+++ test/unit/libraries/cumulativeNormalDistribution.ts
@@ -28,23 +28,30 @@ describe('testCumulativeNormalDistribution', function () {
       let x = 1
       let y = 0.5
       let cdf = Math.floor(std_n_cdf(x) * Wei.Mantissa) / Wei.Mantissa
-      await expect(await cumulative.cdf(x)).to.not.be.reverted
-      await expect(await cumulative.cdf(y)).to.not.be.reverted
+      await expect(cumulative.cdf(parseWei(x).div(1e10).raw)).to.not.be.reverted
+      await expect(cumulative.cdf(parseWei(y).div(1e10).raw)).to.not.be.reverted
       expect(new Integer64x64(await cumulative.cdf(x)).parsed).to.be.closeTo(cdf, precision.percentage)
     })
 
     it('cdf: negative values', async function () {
-      let x = -1
-      let y = -0.5
+      let x = 1
+      let y = 0.5
       let cdf = Math.floor(std_n_cdf(x) * Wei.Mantissa) / Wei.Mantissa
-      await expect(await cumulative.cdfX64(parseInt64x64(x).raw)).to.not.be.reverted
-      await expect(await cumulative.cdfX64(parseInt64x64(y).raw)).to.not.be.reverted
+      await expect(cumulative.cdfX64(parseInt64x64(x).raw)).to.not.be.reverted // flips sign in fn
+      await expect(cumulative.cdfX64(parseInt64x64(y).raw)).to.not.be.reverted // flips sign in fn
       expect(new Integer64x64(await cumulative.cdf(x)).parsed).to.be.closeTo(cdf, precision.percentage)
     })
 
-    it('icdf', async function () {
+    it('icdf: positive value', async function () {
       let x = 0.25
       let icdf = inverse_std_n_cdf(x)
+      expect(new Integer64x64(await cumulative.icdf(parseWei(x).raw)).parsed).to.be.closeTo(icdf, precision.percentage)
+    })
+
+    it('icdf: negative value', async function () {
+      let x = 0.25
+      let icdf = inverse_std_n_cdf(x)
+      await expect(cumulative.icdfX64(parseInt64x64(x).raw)).to.not.be.reverted // flips sign in fn
       expect(new Integer64x64(await cumulative.icdf(parseWei(x).raw)).parsed).to.be.closeTo(icdf, precision.percentage)
     })
   })
