9a150123cf79003bbc14cde79a5662e308308028
alex <alexangel.j@gmail.com>
2021-09-16 15:25:26 -0700

    test(getRisky..): adds higher fidelity tests for getRiskyGivenStable

--- test/unit/libraries/replicationMath.test.ts
+++ test/unit/libraries/replicationMath.test.ts
@@ -68,23 +68,54 @@ async function testStepFixture([wallet]: Wallet[], provider): Promise<TestStepFi
   }
 }
 
-function step3(reserve: Wei) {
+const getStableFns = {
+  riskySwapStep3,
+  riskySwapStep4,
+  riskySwapStep5,
+}
+
+const getRiskyFns = {
+  stableSwapStep3,
+  stableSwapStep4,
+  stableSwapStep5,
+}
+
+function riskySwapStep3(reserve: Wei) {
   let inside = 1 - reserve.float
   let expected = inverse_std_n_cdf(inside)
   return expected
 }
 
-function step4(reserve: Wei, sigma: number, tau: Time) {
+function riskySwapStep4(reserve: Wei, sigma: number, tau: Time) {
   let vol = getProportionalVol(sigma, tau.years)
-  let expected = step3(reserve) - vol
+  let expected = riskySwapStep3(reserve) - vol
   return expected
 }
 
-function step5(reserve: Wei, strike: Wei, sigma: number, tau: Time) {
+function riskySwapStep5(reserve: Wei, strike: Wei, sigma: number, tau: Time) {
   const invariant = 0
-  let input = step4(reserve, sigma, tau)
+  let input = riskySwapStep4(reserve, sigma, tau)
   let cdf = std_n_cdf(input)
   let expected = strike.float * cdf + invariant
+  return expected
+}
+
+function stableSwapStep3(reserve: Wei, strike: Wei) {
+  let inside = reserve.float / strike.float
+  let expected = inverse_std_n_cdf(inside)
+  return expected
+}
+
+function stableSwapStep4(reserve: Wei, strike: Wei, sigma: number, tau: Time) {
+  let vol = getProportionalVol(sigma, tau.years)
+  let expected = stableSwapStep3(reserve, strike) + vol
+  return expected
+}
+
+function stableSwapStep5(reserve: Wei, strike: Wei, sigma: number, tau: Time) {
+  let input = stableSwapStep4(reserve, strike, sigma, tau)
+  let cdf = std_n_cdf(input)
+  let expected = 1 - cdf
   return expected
 }
 
@@ -100,7 +131,7 @@ interface RangeTest {
   }
 }
 
-const rangeTesting: RangeTest = {
+const riskySwapTests: RangeTest = {
   ['step0']: {
     params: [0], // value
     min: 0,
@@ -135,7 +166,7 @@ const rangeTesting: RangeTest = {
     step: 0.01,
     error: maxError.centralInverseCDF,
     parse: parseWei,
-    expected: step3,
+    expected: riskySwapStep3,
   },
   ['testStep4']: {
     params: [0, 1e3, Time.YearInSeconds], // reserve, sigma, tau
@@ -144,7 +175,7 @@ const rangeTesting: RangeTest = {
     step: 0.01,
     error: maxError.centralInverseCDF,
     parse: parseWei,
-    expected: (val: Wei) => step4(val, 0.1, new Time(Time.YearInSeconds)),
+    expected: (val: Wei) => riskySwapStep4(val, 0.1, new Time(Time.YearInSeconds)),
   },
   ['testStep5']: {
     // input
@@ -154,7 +185,7 @@ const rangeTesting: RangeTest = {
     step: 0.01,
     error: maxError.cdf,
     parse: parseWei,
-    expected: (val: Wei) => step5(val, parseWei(10), 0.1, new Time(Time.YearInSeconds)),
+    expected: (val: Wei) => riskySwapStep5(val, parseWei(10), 0.1, new Time(Time.YearInSeconds)),
   },
   ['getStableGivenRisky']: {
     // reserveRisky
@@ -165,6 +196,74 @@ const rangeTesting: RangeTest = {
     error: maxError.cdf,
     parse: parseWei,
     expected: (val: Wei) => getStableGivenRisky(val.float, 10, 0.1, 1),
+  },
+}
+
+const stableSwapTests: RangeTest = {
+  ['step0']: {
+    params: [0], // value
+    min: 0,
+    max: 10000,
+    step: 100,
+    error: 1e-4,
+    parse: parseWei,
+    expected: (val: Wei) => new FixedPointX64(val.mul(FixedPointX64.Denominator).div(parseWei(1)).raw).parsed,
+  },
+  ['step1']: {
+    params: [0, Time.YearInSeconds], // sigma, tau
+    min: 1,
+    max: 1000,
+    step: 10,
+    error: 1e-4,
+    parse: (val: number) => parseWei(val, 4),
+    expected: (val: Wei) => getProportionalVol(val.float, new Time(Time.YearInSeconds).years),
+  },
+  ['step2']: {
+    params: [0], // reserve risky
+    min: 0,
+    max: 10000,
+    step: 100,
+    error: 1e-4,
+    parse: parseWei,
+    expected: (val: Wei) => new FixedPointX64(val.mul(FixedPointX64.Denominator).div(parseWei(1)).raw).parsed,
+  },
+  ['testStep3']: {
+    params: [0, parseWei(10).raw], // reserve
+    min: 0.1,
+    max: 9.9,
+    step: 0.1,
+    error: maxError.centralInverseCDF,
+    parse: parseWei,
+    expected: (val: Wei) => stableSwapStep3(val, parseWei(10)),
+  },
+  ['testStep4']: {
+    params: [0, parseWei(10).raw, 1e3, Time.YearInSeconds], // reserve, sigma, tau
+    min: 0.1,
+    max: 9.9,
+    step: 0.1,
+    error: maxError.centralInverseCDF,
+    parse: parseWei,
+    expected: (val: Wei) => stableSwapStep4(val, parseWei(10), 0.1, new Time(Time.YearInSeconds)),
+  },
+  ['testStep5']: {
+    // input
+    params: [0, parseWei(10).raw, 1e3, Time.YearInSeconds], // reserve, strike, sigma, tau
+    min: 0.1,
+    max: 9.9,
+    step: 0.1,
+    error: maxError.cdf,
+    parse: parseWei,
+    expected: (val: Wei) => stableSwapStep5(val, parseWei(10), 0.1, new Time(Time.YearInSeconds)),
+  },
+  ['getRiskyGivenStable']: {
+    // reserveRisky
+    params: [0, 1, 0, parseWei(10).raw, 1e3, Time.YearInSeconds], // invariant, prec, risky, strike, sigma, tau
+    min: 0.1,
+    max: 9.9,
+    step: 0.1,
+    error: maxError.cdf,
+    parse: parseWei,
+    expected: (val: Wei) => getRiskyGivenStable(val.float, 10, 0.1, 1),
   },
 }
 
@@ -208,9 +307,9 @@ TestPools.forEach(function (pool: PoolState) {
         math = fixture.getStableGivenRisky
       })
 
-      for (let step in rangeTesting) {
+      for (let step in riskySwapTests) {
         describe(`testing ${step}`, function () {
-          let range = rangeTesting[step]
+          let range = riskySwapTests[step]
           let increment: number = range.step
           let value: number = range.min
 
@@ -225,6 +324,38 @@ TestPools.forEach(function (pool: PoolState) {
               }
               const actual = new FixedPointX64(await math[step](...range.params)).parsed
               if (step == 'testStep3' || step == 'testStep4' || step == 'testStep5' || step == 'getStableGivenRisky')
+                console.log(`${step} w/ reserve: ${i}: expected: ${+expected}, actual: ${actual}, ae: ${actual - expected}`)
+              //expect(actual).to.be.closeTo(+expected, range.error)
+            }
+          })
+        })
+      }
+    })
+
+    describe('testGetRiskyGivenStable', function () {
+      let math: TestGetRiskyGivenStable
+
+      beforeEach(async function () {
+        math = fixture.getRiskyGivenStable
+      })
+
+      for (let step in stableSwapTests) {
+        describe(`testing ${step}`, function () {
+          let range = stableSwapTests[step]
+          let increment: number = range.step
+          let value: number = range.min
+
+          it('stays within max error', async function () {
+            for (let i = value; i < range.max; i += increment) {
+              const input = range.parse(i)
+              const expected = range.expected(input) //getProportionalVol(value.float, 1)
+              if (step == 'getRiskyGivenStable') {
+                range.params[2] = input.raw
+              } else {
+                range.params[0] = input.raw
+              }
+              const actual = new FixedPointX64(await math[step](...range.params)).parsed
+              if (step == 'testStep3' || step == 'testStep4' || step == 'testStep5' || step == 'getRiskyGivenStable')
                 console.log(`${step} w/ reserve: ${i}: expected: ${+expected}, actual: ${actual}, ae: ${actual - expected}`)
               //expect(actual).to.be.closeTo(+expected, range.error)
             }
