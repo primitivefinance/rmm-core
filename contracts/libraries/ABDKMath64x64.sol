2dd3cb6a461b4bd437a1413ec4d379af50bb85d9
t11s <greenfeatherhelp@gmail.com>
2021-12-10 21:51:55 -0800

    perf: faster sqrtu from solmate

--- contracts/libraries/ABDKMath64x64.sol
+++ contracts/libraries/ABDKMath64x64.sol
@@ -703,52 +703,53 @@ library ABDKMath64x64 {
      * Calculate sqrt (x) rounding down, where x is unsigned 256-bit integer
      * number.
      *
+     * @author Adapted from Rari-Capital/solmate (FixedPointMathLib.sol)
+     *
      * @param x unsigned 256-bit integer number
      * @return unsigned 128-bit integer number
      */
     function sqrtu(uint256 x) private pure returns (uint128) {
+        if (x == 0) return 0;
+        result = 1;
+
+        uint256 x2 = x;
+        if (x2 >= 0x100000000000000000000000000000000) {
+            x2 >>= 128;
+            result <<= 64;
+        }
+        if (x2 >= 0x10000000000000000) {
+            x2 >>= 64;
+            result <<= 32;
+        }
+        if (x2 >= 0x100000000) {
+            x2 >>= 32;
+            result <<= 16;
+        }
+        if (x2 >= 0x10000) {
+            x2 >>= 16;
+            result <<= 8;
+        }
+        if (x2 >= 0x100) {
+            x2 >>= 8;
+            result <<= 4;
+        }
+        if (x2 >= 0x10) {
+            x2 >>= 4;
+            result <<= 2;
+        }
+        if (x2 >= 0x8) result <<= 1;
+
         unchecked {
-            if (x == 0) return 0;
-            else {
-                uint256 xx = x;
-                uint256 r = 1;
-                if (xx >= 0x100000000000000000000000000000000) {
-                    xx >>= 128;
-                    r <<= 64;
-                }
-                if (xx >= 0x10000000000000000) {
-                    xx >>= 64;
-                    r <<= 32;
-                }
-                if (xx >= 0x100000000) {
-                    xx >>= 32;
-                    r <<= 16;
-                }
-                if (xx >= 0x10000) {
-                    xx >>= 16;
-                    r <<= 8;
-                }
-                if (xx >= 0x100) {
-                    xx >>= 8;
-                    r <<= 4;
-                }
-                if (xx >= 0x10) {
-                    xx >>= 4;
-                    r <<= 2;
-                }
-                if (xx >= 0x8) {
-                    r <<= 1;
-                }
-                r = (r + x / r) >> 1;
-                r = (r + x / r) >> 1;
-                r = (r + x / r) >> 1;
-                r = (r + x / r) >> 1;
-                r = (r + x / r) >> 1;
-                r = (r + x / r) >> 1;
-                r = (r + x / r) >> 1; // Seven iterations should be enough
-                uint256 r1 = x / r;
-                return uint128(r < r1 ? r : r1);
-            }
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+            result = (result + x / result) >> 1;
+
+            uint256 roundedDownResult = x / result;
+            if (result > roundedDownResult) result = roundedDownResult;
         }
     }
 }
