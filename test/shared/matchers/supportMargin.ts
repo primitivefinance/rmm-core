3f8193320d8f76239e349ec1ecbf01dd5a02a658
alexangelj <alexangel.j@gmail.com>
2022-01-11 14:41:35 -0800

    test(matchers): updates custom chai margin matchers

--- test/shared/matchers/supportMargin.ts
+++ test/shared/matchers/supportMargin.ts
@@ -1,5 +1,18 @@
 import { BigNumber } from 'ethers'
-import { EngineTypes } from '../../../types'
+import { Awaited, EngineTypes } from '../../../types'
+
+type EngineMarginsType = Awaited<ReturnType<EngineTypes['margins']>>
+
+async function getMarginChange(
+  transaction: () => Promise<void> | void,
+  engine: EngineTypes,
+  account: string
+): Promise<{ after: EngineMarginsType; before: EngineMarginsType }> {
+  const before = await engine.margins(account)
+  await transaction()
+  const after = await engine.margins(account)
+  return { after, before }
+}
 
 // Chai matchers for the margins of the PrimitiveEngine
 
@@ -7,56 +20,66 @@ export default function supportMargin(Assertion: Chai.AssertionStatic) {
   Assertion.addMethod(
     'increaseMargin',
     async function (this: any, engine: EngineTypes, account: string, delRisky: BigNumber, delStable: BigNumber) {
-      const oldMargin = await engine.margins(account)
-      await this._obj
-      const newMargin = await engine.margins(account)
+      const subject = this._obj
 
-      const expectedRisky = oldMargin.balanceRisky.add(delRisky)
-      const expectedStable = oldMargin.balanceStable.add(delStable)
+      const derivedPromise = Promise.all([getMarginChange(subject, engine, account)]).then(([{ after, before }]) => {
+        const expectedRisky = before.balanceRisky.add(delRisky) // INCREASE
+        const expectedStable = before.balanceStable.add(delStable) // INCREASE
 
-      this.assert(
-        newMargin.balanceRisky.eq(expectedRisky),
-        `Expected ${newMargin.balanceRisky} to be ${expectedRisky}`,
-        `Expected ${newMargin.balanceRisky} NOT to be ${expectedRisky}`,
-        expectedRisky,
-        newMargin.balanceRisky
-      )
+        this.assert(
+          before.balanceRisky.eq(expectedRisky),
+          `Expected ${before.balanceRisky} to be ${expectedRisky}`,
+          `Expected ${before.balanceRisky} NOT to be ${expectedRisky}`,
+          expectedRisky,
+          before.balanceRisky
+        )
 
-      this.assert(
-        newMargin.balanceStable.eq(expectedStable),
-        `Expected ${newMargin.balanceStable} to be ${expectedStable}`,
-        `Expected ${newMargin.balanceStable} NOT to be ${expectedStable}`,
-        expectedStable,
-        newMargin.balanceStable
-      )
+        this.assert(
+          before.balanceStable.eq(expectedStable),
+          `Expected ${before.balanceStable} to be ${expectedStable}`,
+          `Expected ${before.balanceStable} NOT to be ${expectedStable}`,
+          expectedStable,
+          before.balanceStable
+        )
+      })
+
+      this.then = derivedPromise.then.bind(derivedPromise)
+      this.catch = derivedPromise.catch.bind(derivedPromise)
+      this.promise = derivedPromise
+      return this
     }
   )
 
   Assertion.addMethod(
     'decreaseMargin',
     async function (this: any, engine: EngineTypes, account: string, delRisky: BigNumber, delStable: BigNumber) {
-      const oldMargin = await engine.margins(account)
-      await this._obj
-      const newMargin = await engine.margins(account)
+      const subject = this._obj
 
-      const expectedRisky = oldMargin.balanceRisky.sub(delRisky)
-      const expectedStable = oldMargin.balanceStable.sub(delStable)
+      const derivedPromise = Promise.all([getMarginChange(subject, engine, account)]).then(([{ after, before }]) => {
+        const expectedRisky = before.balanceRisky.sub(delRisky) // DECREASE
+        const expectedStable = before.balanceStable.sub(delStable) // DECREASE
 
-      this.assert(
-        newMargin.balanceRisky.eq(expectedRisky),
-        `Expected ${newMargin.balanceRisky} to be ${expectedRisky}`,
-        `Expected ${newMargin.balanceRisky} NOT to be ${expectedRisky}`,
-        expectedRisky,
-        newMargin.balanceRisky
-      )
+        this.assert(
+          before.balanceRisky.eq(expectedRisky),
+          `Expected ${before.balanceRisky} to be ${expectedRisky}`,
+          `Expected ${before.balanceRisky} NOT to be ${expectedRisky}`,
+          expectedRisky,
+          before.balanceRisky
+        )
 
-      this.assert(
-        newMargin.balanceStable.eq(expectedStable),
-        `Expected ${newMargin.balanceStable} to be ${expectedStable}`,
-        `Expected ${newMargin.balanceStable} NOT to be ${expectedStable}`,
-        expectedStable,
-        newMargin.balanceStable
-      )
+        this.assert(
+          before.balanceStable.eq(expectedStable),
+          `Expected ${before.balanceStable} to be ${expectedStable}`,
+          `Expected ${before.balanceStable} NOT to be ${expectedStable}`,
+          expectedStable,
+          before.balanceStable
+        )
+      })
+
+      this.then = derivedPromise.then.bind(derivedPromise)
+      this.catch = derivedPromise.catch.bind(derivedPromise)
+      this.promise = derivedPromise
+      return this
     }
   )
 }
