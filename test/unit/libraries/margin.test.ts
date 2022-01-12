4b7002815e6d26cd88327e91ba9f08e0351c1bcd
alexangelj <alexangel.j@gmail.com>
2022-01-11 22:40:52 -0800

    test(libraries): begin test refactor for libs, failing rn

--- test/unit/libraries/margin.test.ts
+++ test/unit/libraries/margin.test.ts
@@ -1,14 +1,24 @@
+import { ethers } from 'hardhat'
 import { parseWei } from 'web3-units'
 
 import expect from '../../shared/expect'
-import { libraryFixture } from '../../shared/fixtures'
+import { librariesFixture, libraryFixture } from '../../shared/fixtures'
 import { testContext } from '../../shared/testContext'
 
 import { TestMargin } from '../../../typechain'
+import { createFixtureLoader } from 'ethereum-waffle'
+import { Wallet } from 'ethers'
 
 testContext('testMargin', function () {
+  let loadFixture: ReturnType<typeof createFixtureLoader>
+  let signer: Wallet, other: Wallet
+  before(async function () {
+    ;[signer, other] = await (ethers as any).getSigners()
+    loadFixture = createFixtureLoader([signer, other])
+  })
+
   beforeEach(async function () {
-    const fixture = await this.loadFixture(libraryFixture)
+    const fixture = await loadFixture(librariesFixture)
     this.libraries = fixture.libraries
   })
 
