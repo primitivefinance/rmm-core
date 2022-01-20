a77517629e74ccb2a41171cf6055b4eb1b3ec962
alexangelj <root@Godfather.localdomain>
2022-01-20 11:38:06 -0800

    fix(tob-ptv-6): remove unused int128.percentage() fn

--- rmm-core/contracts/LibraryMathEchidna.sol
+++ rmm-core/contracts/LibraryMathEchidna.sol
@@ -5,55 +5,47 @@ import "./libraries/ReplicationMath.sol";
 import "./libraries/Units.sol";
 
 // npx hardhat clean && npx hardhat compile && echidna-test-2.0 . --contract LibraryMathEchidna --test-mode assertion
-contract LibraryMathEchidna { 
-	event AssertionFailed(string functionName, int128 v1, uint256 v2); 
-	using ReplicationMath for int128;
-	using Units for int128;
-	using Units for uint256;
+contract LibraryMathEchidna {
+    event AssertionFailed(string functionName, int128 v1, uint256 v2);
+    using ReplicationMath for int128;
+    using Units for int128;
+    using Units for uint256;
 
-	// Helper Functions for ReplicationMath.sol
-	function realisticSigma(uint256 sigma) internal pure returns(uint256) { 
-		// between 1 to 1e7 
-		return uint256(1 + sigma % (1e7 - 1));
-	}
-	function realisticGamma(uint256 sigma ) internal pure returns(uint256) { 
-		// between 9000 to Units.PERCENTAGE
-		return uint256(9000 + sigma % (Units.PERCENTAGE - 9000));
-	}
-	function realisticAmountIncluding0ne(uint256 amount) internal pure returns(uint256) { 
-		// between 1 - 100000 ether
-		return uint256(1 + amount% (100000 ether + 1));
-	}
+    // Helper Functions for ReplicationMath.sol
+    function realisticSigma(uint256 sigma) internal pure returns (uint256) {
+        // between 1 to 1e7
+        return uint256(1 + (sigma % (1e7 - 1)));
+    }
 
-	// --------------------- Units.sol -----------------------
-	function scaleUpAndScaleDownInverses(uint256 value, uint256 factor) public {
-		uint256 scaledFactor = (10e18+ factor % (10e18 + 1));
+    function realisticGamma(uint256 sigma) internal pure returns (uint256) {
+        // between 9000 to Units.PERCENTAGE
+        return uint256(9000 + (sigma % (Units.PERCENTAGE - 9000)));
+    }
 
-		uint256 scaledUpValue = value.scaleUp(scaledFactor);
-		uint256 scaledDownValue = scaledUpValue.scaleDown(scaledFactor);
-		
-		assert(scaledDownValue == value);
-	}
-	function scaleToAndFromX64Inverses(uint256 value, uint256 _decimals) public {
-		// will enforce factor between 0 - 12
-		uint256 factor = _decimals % (13); 
-		// will enforce scaledFactor between 1 - 10**12 , because 10**0 = 1
-		uint256 scaledFactor = 10**factor;
+    function realisticAmountIncluding0ne(uint256 amount) internal pure returns (uint256) {
+        // between 1 - 100000 ether
+        return uint256(1 + (amount % (100000 ether + 1)));
+    }
 
-		int128 scaledUpValue = value.scaleToX64(scaledFactor);
-		uint256 scaledDownValue = scaledUpValue.scalefromX64(scaledFactor);
-		
-		assert(scaledDownValue == value);
-	}
-	function scalePercentages(uint256 value) public {
-		require(value > Units.PERCENTAGE);
-		int128 signedPercentage =  value.percentage();
-		uint256 unsignedPercentage = signedPercentage.percentage();
-		
-		if(unsignedPercentage != value) {
-			emit AssertionFailed("scalePercentages", signedPercentage, unsignedPercentage);
-			assert(false);
-		}
-		
-	}
+    // --------------------- Units.sol -----------------------
+    function scaleUpAndScaleDownInverses(uint256 value, uint256 factor) public {
+        uint256 scaledFactor = (10e18 + (factor % (10e18 + 1)));
+
+        uint256 scaledUpValue = value.scaleUp(scaledFactor);
+        uint256 scaledDownValue = scaledUpValue.scaleDown(scaledFactor);
+
+        assert(scaledDownValue == value);
+    }
+
+    function scaleToAndFromX64Inverses(uint256 value, uint256 _decimals) public {
+        // will enforce factor between 0 - 12
+        uint256 factor = _decimals % (13);
+        // will enforce scaledFactor between 1 - 10**12 , because 10**0 = 1
+        uint256 scaledFactor = 10**factor;
+
+        int128 scaledUpValue = value.scaleToX64(scaledFactor);
+        uint256 scaledDownValue = scaledUpValue.scalefromX64(scaledFactor);
+
+        assert(scaledDownValue == value);
+    }
 }
