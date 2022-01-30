01fba41fcf40fc7f36c7cdf68c5fd0aa5649ff57
alexangelj <alexangel.j@gmail.com>
2022-01-30 14:31:49 -0800

    fix(#275): updates natspec of all interfaces

--- contracts/interfaces/engine/IPrimitiveEngineView.sol
+++ contracts/interfaces/engine/IPrimitiveEngineView.sol
@@ -6,8 +6,8 @@ pragma solidity >=0.5.0;
 interface IPrimitiveEngineView {
     // ===== View =====
 
-    /// @notice             Fetches the current invariant based on risky and stable token reserves of pool with `poolId`
-    /// @param  poolId      Pool Identifier
+    /// @notice             Fetches the current invariant, notation is usually `k`, based on risky and stable token reserves of pool with `poolId`
+    /// @param  poolId      Keccak256 hash of the engine address, strike, sigma, maturity, and gamma
     /// @return invariant   Signed fixed point 64.64 number, invariant of `poolId`
     function invariantOf(bytes32 poolId) external view returns (int128 invariant);
 
@@ -27,10 +27,10 @@ interface IPrimitiveEngineView {
     //// @return Factory address which deployed this engine contract
     function factory() external view returns (address);
 
-    //// @return Risky token address
+    //// @return Risky token address, a more accurate name is the underlying token
     function risky() external view returns (address);
 
-    /// @return Stable token address
+    /// @return Stable token address, a more accurate name is the quote token
     function stable() external view returns (address);
 
     /// @return Multiplier to scale amounts to/from, equal to 10^(18 - riskyDecimals)
@@ -42,7 +42,7 @@ interface IPrimitiveEngineView {
     // ===== Pool State =====
 
     /// @notice                      Fetches the global reserve state for a pool with `poolId`
-    /// @param  poolId               Pool Identifier
+    /// @param  poolId               Keccak256 hash of the engine address, strike, sigma, maturity, and gamma
     /// @return reserveRisky         Risky token balance in the reserve
     /// @return reserveStable        Stable token balance in the reserve
     /// @return liquidity            Total supply of liquidity for the curve
@@ -64,12 +64,12 @@ interface IPrimitiveEngineView {
         );
 
     /// @notice                 Fetches `Calibration` pool parameters
-    /// @param  poolId          Pool Identifier
-    /// @return strike          Strike price of the pool with stable token decimals
-    /// @return sigma           Implied Volatility as an unsigned 32-bit integer constant w/ precision of 1e4, 10000 = 100%
-    /// @return maturity        Timestamp of maturity in seconds
+    /// @param  poolId          Keccak256 hash of the engine address, strike, sigma, maturity, and gamma
+    /// @return strike          Marginal price of the pool's risky token at maturity, with the same decimals as the stable token, valid [0, 2^128-1]
+    /// @return sigma           AKA Implied Volatility in basis points, determines the price impact of swaps, valid for (1, 10_000_000)
+    /// @return maturity        Timestamp which starts the BUFFER countdown until swaps will cease, in seconds, valid for (block.timestamp, 2^32-1]
     /// @return lastTimestamp   Last timestamp used to calculate time until expiry, aka "tau"
-    /// @return gamma           = 1 - fee %, as an unsigned 32-bit integer constant w/ precision of 1e4, 10000 = 100%
+    /// @return gamma           Multiplied against swap in amounts to apply fee, equal to 1 - fee % but units are in basis points, valid for (9_000, 10_000)
     function calibrations(bytes32 poolId)
         external
         view
@@ -82,7 +82,7 @@ interface IPrimitiveEngineView {
         );
 
     /// @notice             Fetches position liquidity an account address and poolId
-    /// @param  poolId      Pool Identifier
+    /// @param  poolId      Keccak256 hash of the engine address, strike, sigma, maturity, and gamma
     /// @return liquidity   Liquidity owned by `account` in `poolId`
     function liquidity(address account, bytes32 poolId) external view returns (uint256 liquidity);
 
